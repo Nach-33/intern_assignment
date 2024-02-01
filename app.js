9ad2379b10a109b11a5d90beb493893f3457f3a4
Nachiket <nachiketdodia33@gmail.com>
2024-02-01 13:24:02 +0530

    added is_ready attribute

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ msg: "API working successfully" });
+  res.json({ msg: "API working successfully", is_ready: true });
 });
 
 app.post("/getWeather", async (req, res) => {
